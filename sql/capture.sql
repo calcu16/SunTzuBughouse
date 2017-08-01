215b5029ee6e30128832a111c56a7ccb9b0747c9
Andrew Carter <acarter@linkedin.com>
2017-08-01 10:54:08 -0700

    fixing bug in update_locations

--- sql/capture.sql
+++ sql/capture.sql
@@ -2,7 +2,7 @@ update locations
   set l_lid=:tomb, l_visible=0, l_bid=1-l_bid, l_pid=l_bpid
   where l_lid=:elid
     and exists (
-      select * from entrants 
+      select * from entrants
       join users on e_uid=u_uid and e_bid=l_bid
       join boards on b_bid=l_bid and e_sid=b_sid
       where l_gid=e_gid and u_value=:uid and e_gid=:gid);
--- sql/update_location.sql
+++ sql/update_location.sql
@@ -1,10 +1,10 @@
 update locations
   set l_lid=:elid, l_pid=CASE WHEN
-    l_pid=5 and CAST(:elid as INTEGER) >= 0 and CAST(:elid as INTEGER) < 8 or CAST(:elid as INTEGER) >= 56 and CAST(:elid as INTEGER) < 64
+    l_pid=5 and (CAST(:elid as INTEGER) >= 0 and CAST(:elid as INTEGER) < 8 or CAST(:elid as INTEGER) >= 56 and CAST(:elid as INTEGER) < 64)
     THEN 1
     ELSE l_pid END
   where l_lid=:slid and l_gid=:gid and exists (
-    select * from entrants 
+    select * from entrants
   join users on u_uid=e_uid
   join boards on b_bid=l_bid and b_sid=e_sid and b_cid=l_cid
   where e_gid=l_gid and e_bid=l_bid and u_value=:uid);
